73ddcd859c15e5bd360edb08c8bfcd8fefc03d5f
Jhon Gómez <jhongomez100@gmail.com>
2021-03-11 08:41:56 -0500

    lista receta

--- header.php
+++ header.php
@@ -1,4 +1,28 @@
     <header>
-        <h1>Esta es una receta</h1>
+        <h1>Receta  para hacer albóndigas</h1>
+        <hr>
+        <ul>
+          <li>750 gramos de carne picada (ternera y cerdo)</li>
+          <li> 2 dientes de ajos</li>
+          <li> 1 huevo</li>
+          <li>1 puñado de perejil</li>
+          <li>1 pizca de sal</li>
+          <li>1 pizca de pimienta</li>
+          <li> 2 cebollas</li>
+          <li>150 mililitros de vino blanco</li>
+          <li>1 chorro de aceite de oliva</li>
+          <li>100 gramos de harina de arroz o maicena</li>
+        </ul>
+        <hr>
+        <h2>Sigue el paso a paso</h2>
+        <ol>
+          <li> </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
 
-    </header>
+        </ol>
+  </header>
