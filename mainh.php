e85be8327cf9a9a4e52d1b400c1cb392bc1ac831
Jhon Gómez <jhongomez100@gmail.com>
2021-03-11 08:48:11 -0500

    receta albondiga

---
+++ mainh.php
@@ -0,0 +1,26 @@
+<main>
+  <ul>
+    <li>750 gramos de carne picada (ternera y cerdo)</li>
+    <li> 2 dientes de ajos</li>
+    <li> 1 huevo</li>
+    <li>1 puñado de perejil</li>
+    <li>1 pizca de sal</li>
+    <li>1 pizca de pimienta</li>
+    <li> 2 cebollas</li>
+    <li>150 mililitros de vino blanco</li>
+    <li>1 chorro de aceite de oliva</li>
+    <li>100 gramos de harina de arroz o maicena</li>
+  </ul>
+  <hr>
+  <h2>Sigue el paso a paso</h2>
+  <ol>
+    <li> </li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+
+  </ol>
+</main>
